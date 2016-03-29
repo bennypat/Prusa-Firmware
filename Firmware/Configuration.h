a61d4caea0aed66c9030f07b7adfda6a3ac66f7e
bennypat <pattison.ben@gmail.com>
2016-03-28 20:49:25 -0700

    Update Configuration.h
    Increased bed temp proportional gain by 25%

--- Firmware/Configuration.h
+++ Firmware/Configuration.h
@@ -158,7 +158,7 @@
 #ifdef PIDTEMPBED
 //120v 250W silicone heater into 4mm borosilicate (MendelMax 1.5+)
 //from FOPDT model - kp=.39 Tp=405 Tdead=66, Tc set to 79.2, aggressive factor of .15 (vs .1, 1, 10)
-    #define  DEFAULT_bedKp 10.00
+    #define  DEFAULT_bedKp 12.50
     #define  DEFAULT_bedKi .023
     #define  DEFAULT_bedKd 305.4
 
